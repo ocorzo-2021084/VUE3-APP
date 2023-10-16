30818bf200d6890257c028c1ae659b153503d451
Octavio Corzo <ocorzo-2021084@kinal.edu.gt>
2023-10-16 15:40:08 -0600

    Form update user

--- src/views/Perfil.vue
+++ src/views/Perfil.vue
@@ -1,7 +1,40 @@
 <template>
     <h1>Perfil de usuario</h1>
+    <a-row>
+        <a-col :xs="{ span: 24 }" :sm="{ span: 12, offset: 6 }">
+            <!-- <a-card> -->
+            <a-form name="basicPerfil" autocomplete="off" layout="vertical" :model="userStore.userData" @finish="onFinish">
+                <a-form-item name="email" label="Tu correo (no modificable)" :rules="[
+                    {
+                        whitespace: true,
+                        type: 'email',
+                        message: 'Ingresa un email válido',
+                    },
+                ]">
+                    <a-input disabled v-model:value="userStore.userData.email"></a-input>
+                </a-form-item>
+                <a-form-item name="displayName" label="Ingrese su nickname" :rules="[
+                    {
+                        required: true,
+                        whitespace: true,
+                        message: 'Ingresa un nickname válido',
+                    },
+                ]">
+                    <a-input v-model:value="userStore.userData.displayName"></a-input>
+                </a-form-item>
+                <a-form-item>
+                    <a-button type="primary" html-type="submit" :disabled="userStore.loadingUser"
+                        :loading="userStore.loadingUser">Actualizar información</a-button>
+                </a-form-item>
+            </a-form>
+            <!-- </a-card> -->
+        </a-col>
+    </a-row>
 </template>
 
 <script setup>
+import { useUserStore } from '../stores/user';
 
+const userStore = useUserStore();
+const onFinish = (value) => {};
 </script>
